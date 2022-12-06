06084bab45d934f5980f66d3b0e1a39f9d90ad4d
Aymen HECHMI <ay.hechmi@gmail.com>
2022-12-06 10:41:10 +0100

    Initial Commit

---
+++ JetbrainsProjects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JetbrainsProjects
+//
+//  Created by Aymen HECHMI on 6/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
